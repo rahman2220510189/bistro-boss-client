ddf3fc25bb564194bd76d357798fa0c9f3ca3c11
rahman2220510189 <rahman22205101894@diu.edu.bd>
2026-03-22 13:53:13 +0600

    this is the last commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,32 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-
-import {
-
-  RouterProvider,
-} from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 import { router } from './Rouets/Routes';
 import { HelmetProvider } from 'react-helmet-async';
 import AuthProvider from './Providers/AuthProvider';
-import {
-  QueryClient,
-  QueryClientProvider,
-  
-} from '@tanstack/react-query'
-const queryClient = new QueryClient()
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
+const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
-    <QueryClientProvider client={queryClient}>
-    <HelmetProvider>
-        <div className='max-w-screen-xl mx-auto'>
+      <QueryClientProvider client={queryClient}>
+        <HelmetProvider>
           <RouterProvider router={router} />
-        </div>
-      </HelmetProvider>
-    </QueryClientProvider>
+        </HelmetProvider>
+      </QueryClientProvider>
     </AuthProvider>
   </StrictMode>,
 )
